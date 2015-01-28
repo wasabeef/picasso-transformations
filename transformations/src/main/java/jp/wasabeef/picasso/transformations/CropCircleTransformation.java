b1c20499bd604d7d55ec8926354a7641ef10e3bc
pushbit <pushbit@gmail.com>
2015-01-28 15:41:55 +0100

    centring source by Matrix instead of a new Bitmap

--- transformations/src/main/java/jp/wasabeef/picasso/transformations/CropCircleTransformation.java
+++ transformations/src/main/java/jp/wasabeef/picasso/transformations/CropCircleTransformation.java
@@ -16,12 +16,13 @@ package jp.wasabeef.picasso.transformations;
  * limitations under the License.
  */
 
-import com.squareup.picasso.Transformation;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+
+import com.squareup.picasso.Transformation;
 
 public class CropCircleTransformation implements Transformation {
 
@@ -32,24 +33,22 @@ public class CropCircleTransformation implements Transformation {
         int width = (source.getWidth() - size) / 2;
         int height = (source.getHeight() - size) / 2;
 
-        Bitmap squaredBitmap = Bitmap.createBitmap(source, width, height, size, size);
-        if (squaredBitmap != source) {
-            source.recycle();
-        }
-
         Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
 
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
-        BitmapShader shader = new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP,
+        BitmapShader shader = new BitmapShader(source, BitmapShader.TileMode.CLAMP,
                 BitmapShader.TileMode.CLAMP);
+        Matrix matrix = new Matrix();
+        matrix.setTranslate(-width, -height);
+        shader.setLocalMatrix(matrix);
         paint.setShader(shader);
         paint.setAntiAlias(true);
 
         float r = size / 2f;
         canvas.drawCircle(r, r, r, paint);
 
-        squaredBitmap.recycle();
+        source.recycle();
 
         return bitmap;
     }

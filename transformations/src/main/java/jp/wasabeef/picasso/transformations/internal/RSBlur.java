ae7e4b6cb55b42196aa51428edab3f3588259f3a
wasabeef <dadadada.chop@gmail.com>
2017-03-17 18:16:08 -0700

    fix leaks

--- transformations/src/main/java/jp/wasabeef/picasso/transformations/internal/RSBlur.java
+++ transformations/src/main/java/jp/wasabeef/picasso/transformations/internal/RSBlur.java
@@ -31,13 +31,15 @@ public class RSBlur {
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
   public static Bitmap blur(Context context, Bitmap bitmap, int radius) throws RSRuntimeException {
     RenderScript rs = null;
+    Allocation input = null;
+    Allocation output = null;
+    ScriptIntrinsicBlur blur = null;
     try {
       rs = RenderScript.create(context);
-      Allocation input =
-          Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE,
-              Allocation.USAGE_SCRIPT);
-      Allocation output = Allocation.createTyped(rs, input.getType());
-      ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+      input = Allocation.createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE,
+          Allocation.USAGE_SCRIPT);
+      output = Allocation.createTyped(rs, input.getType());
+      blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
 
       blur.setInput(input);
       blur.setRadius(radius);
@@ -46,6 +48,15 @@ public class RSBlur {
     } finally {
       if (rs != null) {
         rs.destroy();
+      }
+      if (input != null) {
+        input.destroy();
+      }
+      if (output != null) {
+        output.destroy();
+      }
+      if (blur != null) {
+        blur.destroy();
       }
     }
 

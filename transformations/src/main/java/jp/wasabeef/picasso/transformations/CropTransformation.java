df9ae5d6235cda71e1f890ea92d40f24ccf1cb28
wasabeef <dadadada.chop@gmail.com>
2020-09-15 02:07:40 +0900

    update copyright

--- transformations/src/main/java/jp/wasabeef/picasso/transformations/CropTransformation.java
+++ transformations/src/main/java/jp/wasabeef/picasso/transformations/CropTransformation.java
@@ -1,7 +1,7 @@
 package jp.wasabeef.picasso.transformations;
 
 /**
- * Copyright (C) 2018 Wasabeef, molexx
+ * Copyright (C) 2020 Wasabeef, molexx
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

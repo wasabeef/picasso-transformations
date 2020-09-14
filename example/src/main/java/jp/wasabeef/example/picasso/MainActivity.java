122a4c36cbc1bb84b88daeae7672b3557d173cdc
Riccardo Ciovati <rciovati@gmail.com>
2020-09-14 18:32:39 +0200

    Housekeeping (#45)
    - Use latest Gradle (6.6) and Android Gradle Plugin (4.0.1)
    - Use latest Picasso (2.8)
    - Use AndroidX

--- example/src/main/java/jp/wasabeef/example/picasso/MainActivity.java
+++ example/src/main/java/jp/wasabeef/example/picasso/MainActivity.java
@@ -1,9 +1,9 @@
 package jp.wasabeef.example.picasso;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 import jp.wasabeef.example.picasso.MainAdapter.Type;
